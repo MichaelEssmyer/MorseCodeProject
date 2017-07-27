1e9de7a597dbf7b3ea5983361e55a680292104df
Michael Essmyer <mde9rb@mail.umkc.edu>
2017-07-26 21:28:05 -0500

    Update Morse.h

--- Morse.h
+++ Morse.h
@@ -9,8 +9,8 @@ class Morse {
 public: 
 	//default constructor
 	Morse()
-	{	//no input thus each node is equal to \0
-		treeCode = Binary_Tree<char>('\0');
+	{	//no input thus each node is equal to NULL
+		treeCode = Binary_Tree<char>(NULL);
 	}
 	Morse(ifstream theStream) 
 	{	//each 
@@ -19,11 +19,12 @@ public:
 	}
 
 	Morse treeCodeSet(istream& theStream) {
-		while (!theStream.eof()) {
+		while (!theStream.eof()) { //while the stream is not at the end of the file
 			count++;
 			string lineTemp;
 			getline(theStream, lineTemp); //convert istream to string
-			if (checkCode(lineTemp)) addMorseCode(lineTemp); //add the string of code
+			if (checkCode(lineTemp)) //if 
+				addMorseCode(lineTemp); //add the string of code
 			theSymbols = "";
 			
 		}
@@ -82,26 +83,31 @@ public:
 
 private:
 	bool addRecurs(BTNode <char>* theNode, string theCode, int index, char theData) {
-		if (index == theCode.length()) {
-			if (theNode->data != '\0')
+		if (index == theCode.length()) { //if the index is at the end aka last step of recursion
+			if (theNode->data != NULL)
 			{
 				cout << "This node contains code that has already been used before" << endl;
 				return false;
 			}
 			else
 			{
-				theNode->data = theData;
-				return true;
+				theNode->data = theData; //the data of the node equals the character of the data
+				return true; //it is true that the data was successfully added
 			}
 		}
-		else {
+		else { //if the index is not at the end of the string
 			char codeSeg = theCode[index];
-			if (Dots.find(codeSeg) != string::npos && theNode->left == NULL) 
+			if (Dots.find(codeSeg) != string::npos && theNode->left == NULL) //if the code segment is a dot and the left node is NULL
 			{
-				theNode->left = new BTNode <char>('\0');
-				theNode = theNode->right;
+				theNode->left = new BTNode <char>(codeSeg); //create a new BTNode to the left with info
+				theNode = theNode->right; //
 			}
-			return addRecurs(theNode, theCode, index + 1, theData);
+			else if (Dashes.find(codeSeg) != string::npos && theNode->right == NULL) //if the code segment is a dash and the right node is NULL
+			{
+				theNode->right = new BTNode <char>(codeSeg); //create a new BTNode to the right with info
+				theNode = theNode->right; //
+			}
+			return addRecurs(theNode, theCode, index + 1, theData); //move to the next item in the string
 		}
 	}
 
@@ -109,7 +115,7 @@ private:
 		char theData = tolower(theInput[0]);
 		string theCode = theInput.substr(2, 4);
 		if (theSymbols.find(theData) == string::npos && addRecurs(theData, theCode, 0, treeCode.getRoot())) { theSymbols += theData; }
-		else cout << "This symbold has already been added to the tree." << endl;
+		else cout << "This symbol has already been added to the tree." << endl;
 	}
 
 	bool checkCode(string theInput) 
@@ -118,10 +124,10 @@ private:
 		else if (theInput.length() > 5) { cout << "This line is too long. PLease try again." << endl; return false;	}
 		else if (theInput[0] == " ";) { cout << "This line does not begin with any symbol. Please try again." << endl; return false; }
 		else if (Alphabet.find(theInput[0]).length() > 5) { cout << "This line contains invalid characters. Please try again." << endl: return false; }
-		for (string::iterator itr = input.begin(); its != theInput.end(); ++itr)
+		for (string::iterator itr = input.begin(); itr != theInput.end(); ++itr)
 		{
 			if (Dashes.find(*itr) != -1) current = current->right;
-			else if (Dots.find(*its) != -1) current = current->left;
+			else if (Dots.find(*itr) != -1) current = current->left;
 			else if (itr == " ") 
 			{
 				if (current != treeCode.getRoot())
@@ -134,13 +140,14 @@ private:
 		}
 	}
 
-	void addMorseCode(string theInput) 
+	void addMorseCode(string theInput) //takes in a string || *-**- or absdja
 	{
-		BTNode<char> theData = tolower(theInput[0]);
-		string theCode = theInput.substr(2, 4);
-		if(theSymbols.find(theData) == string::npos)
+		BTNode<char> theData = tolower(theInput[0]); //lowercase the first index of the string save it to BTNode
+		string theCode = theInput.substr(2, 4); //the rest of the string is saved to theCode
+		if(theSymbols.find(theData) == string::npos)//if the data is a symbol dot/dash
 		{
-			if (addRecurs(theData, theCode, 0, treeCode.getRoot())) theSymbols += theData;
+			if (addRecurs(theData, theCode, 0, treeCode.getRoot())) //if the data is added recursively
+				theSymbols += theData; //add the data to the tree
 
 		}
 		else cout << "This symbol has already been added to the tree." << endl;
